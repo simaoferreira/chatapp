2a5f5ec28f3b2b9224033a1076d2e7e506973a0a
Simão Ferreira <simaoferreira97@gmail.com>
2019-01-05 22:47:02 +0000

    File Client.java cleaned

--- Launcher/src/Client/Client.java
+++ Launcher/src/Client/Client.java
@@ -1,96 +1,92 @@
 package Client;
 
-
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import Controllers.ControllerLauncher;
 import DataHandler.AlertBox;
-import javafx.application.Platform;
-
 
 public class Client extends Thread{
-	
+    
+    private ClientHandler ch;
+    private String user;
+    private Socket socket;
+    private ControllerLauncher client;
+    private static final String ip = "81.84.174.114";
 
-	ClientHandler ch;
-	String user;
-	Socket socket;
-	ControllerLauncher client;
-	private static final String ip = "81.84.174.114";
-		
-	public Client(ControllerLauncher controllerLauncher) {
-		this.client = controllerLauncher;
-	}
+    public Client(ControllerLauncher controllerLauncher) {
+        this.client = controllerLauncher;
+    }
 
-	public void runConnection() throws UnknownHostException, IOException {
-	    socket = new Socket(ip,32456);
-		socket.setTcpNoDelay(true);
-		ch = new ClientHandler(socket,this,client);
-		ch.start();  	
-	}
-	
-	////////////////  CODE 0  ////////////////
-	public void requestUpdateConnections(String user, String pass) {
-		this.user=user;
-		String code = "0";
-		ch.sendMessage(code,user,pass);
-	}
-	
-	////////////////  CODE 1  ////////////////
-	public void sendMessagesToChat(String user) {
-		String inputText = client.text;
-		String code = "1";
-		ch.sendMessage(code,user,inputText);
-	}
-	
-	////////////////  CODE 2  ////////////////
-	public void requestCloseConnection() {
-		String code = "2";
-		ch.sendMessage(code, user, "null");
-	}
-	
-	////////////////  CODE 3  ////////////////
-	public void requestClearChat() {
-		String code = "3";
-		ch.sendMessage(code, "null", "null");
-	}
-	
-	
-	////////////////  CODE 5  ////////////////
-	public void sendPrivateMessageToChat(String user) {
-		String textField = client.text;
-		int index = textField.indexOf("\"");
-		String userTarget = textField.substring(6, index-1);
-		if(userTarget.equals(client.user) || userTarget.equals(client.id)) {
-			ch.printError("You can't send message to yourself!");
-		}else {
-			String text = textField.substring(index+1, textField.length()-1);
-			String users = user+":"+userTarget;
-			String code = "5";
-			ch.sendMessage(code,users,text);
-		}
+    public void runConnection() throws UnknownHostException, IOException {
+        socket = new Socket(ip,32456);
+        socket.setTcpNoDelay(true);
+        ch = new ClientHandler(socket,this,client);
+        ch.start();  	
+    }
 
-	}
+    ////////////////  CODE 0  ////////////////
+    public void requestUpdateConnections(String user, String pass) {
+        this.user=user;
+        String code = "0";
+        ch.sendMessage(code,user,pass);
+    }
 
-	public void replyPrivateMessageToChat(String user, String lastUserPrivate) {
-		if(lastUserPrivate.equals("")) {
-			ch.printError("You can't reply! Wait for a new message");
-		}else {
-			String textField = client.text;
-			String text = textField.substring(3, textField.length());
-			String users = user+":"+lastUserPrivate;
-			String code = "5";
-			ch.sendMessage(code, users, text);
-		}
-		
-		
-		
-	}
+    ////////////////  CODE 1  ////////////////
+    public void sendMessagesToChat(String user) {
+        String inputText = client.text;
+        String code = "1";
+        ch.sendMessage(code,user,inputText);
+    }
 
-	public void printError(String string) {
-		ch.printError(string);
-		
-	}
+    ////////////////  CODE 2  ////////////////
+    public void requestCloseConnection() {
+        String code = "2";
+        ch.sendMessage(code, user, "null");
+    }
+
+    ////////////////  CODE 3  ////////////////
+    public void requestClearChat() {
+        String code = "3";
+        ch.sendMessage(code, "null", "null");
+    }
+
+
+    ////////////////  CODE 5  ////////////////
+    public void sendPrivateMessageToChat(String user) {
+        String textField = client.text;
+        int index = textField.indexOf("\"");
+        String userTarget = textField.substring(6, index-1);
+        if(userTarget.equals(client.user) || userTarget.equals(client.id)) {
+            ch.printError("You can't send message to yourself!");
+        }else {
+            String text = textField.substring(index+1, textField.length()-1);
+            String users = user+":"+userTarget;
+            String code = "5";
+            ch.sendMessage(code,users,text);
+        }
+
+    }
+
+    public void replyPrivateMessageToChat(String user, String lastUserPrivate) {
+        if(lastUserPrivate.equals("")) {
+            ch.printError("You can't reply! Wait for a new message");
+        }else {
+            String textField = client.text;
+            String text = textField.substring(3, textField.length());
+            String users = user+":"+lastUserPrivate;
+            String code = "5";
+            ch.sendMessage(code, users, text);
+        }
+
+
+
+    }
+
+    public void printError(String string) {
+        ch.printError(string);
+
+    }
 
     public void requestInfoUser() {
         System.out.println(client.lvlUser);
@@ -101,7 +97,7 @@ public class Client extends Thread{
                 " Number of words written: "+client.numWordsWritten+"\r\n";
         AlertBox.display("Details", text,false);
     }
-	
-	
-	
+
+
+
 }
